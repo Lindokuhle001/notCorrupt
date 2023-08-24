bec79d1fe0e6e24a3d9f94e246a657bdf6395e60
lindokuhle shabalala <mlixshaba@gmail.com>
2023-08-24 16:15:57 +0200

    init form

---
+++ form/src/components/Form/Form.tsx
@@ -0,0 +1,73 @@
+import React, { useState } from 'react';
+import "./Form.css"
+
+interface FormValues {
+  email: string;
+  password: string;
+  confirmPassword: string;
+}
+
+const initialFormValues: FormValues = {
+  email: '',
+  password: '',
+  confirmPassword: '',
+};
+
+const MyForm: React.FC = () => {
+  const [formValues, setFormValues] = useState<FormValues>(initialFormValues);
+
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const { name, value } = e.target;
+    setFormValues((prevValues) => ({
+      ...prevValues,
+      [name]: value,
+    }));
+  };
+
+  const handleSubmit = (e: React.FormEvent) => {
+    e.preventDefault();
+    // You can add your form submission logic here
+    console.log('Form submitted:', formValues);
+  };
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <div>
+        <label htmlFor="email">Email:</label>
+        <input
+          type="email"
+          id="email"
+          name="email"
+          value={formValues.email}
+          onChange={handleChange}
+          required
+        />
+      </div>
+      <div>
+        <label htmlFor="password">Password:</label>
+        <input
+          type="password"
+          id="password"
+          name="password"
+          value={formValues.password}
+          onChange={handleChange}
+          required
+        />
+      </div>
+      <div>
+        <label htmlFor="confirmPassword">Confirm Password:</label>
+        <input
+          type="password"
+          id="confirmPassword"
+          name="confirmPassword"
+          value={formValues.confirmPassword}
+          onChange={handleChange}
+          required
+        />
+      </div>
+      <button type="submit">Submit</button>
+    </form>
+  );
+};
+
+export default MyForm;
